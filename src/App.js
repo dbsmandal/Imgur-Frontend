29060b02ffe6d970847d193919ed8fe5d41d42d7
dbsmandal <dipakmandaljio420@gmail.com>
2022-11-12 01:10:34 +0530

    Add tailwind and create responsive navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from './Component/Navbar';
+import Home from "./Component/Home";
+import Upload from "./Component/Upload";
+import Search from './Component/Search';
+import Signin from "./Component/Signin";
+import Signup from "./Component/Signup";
+
 
 const App = () => {
   return (
-    <div>
-      Imgur app
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/upload' element={<Upload />} />
+          <Route path='/search' element={<Search />} />
+          <Route path='/signin' element={<Signin />} />
+          <Route path='/signup' element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+
+
+
+
+    </>
   )
 }
 
---
+++ src/Component/Navbar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from "../Images/Imgur_logo.svg.png"
+
+const Navbar = () => {
+  return (
+    <div className='bg-green-300'>
+    <div className=' w-full mx-1 flex gap-4'>
+        <div className='w-2/12 grid place-items-center '>
+            <Link to='/'>
+            <img className='h-10 w-20' src={logo} alt="Imgur-FrontEnd" />
+            </Link>
+
+        </div>
+        <div className='w-10/12 grid place-items-center grid-cols-6 gap-4' >
+            <div>
+                <Link to="/">Home</Link>
+            </div>
+            <div>
+                <Link to="/upload">Upload</Link>
+            </div>
+            <div className='col-span-2'>
+                <Link to="/search">Search</Link>
+            </div>
+            <div>
+                <Link to="/signin">SignIn</Link>
+            </div>
+            <div>
+                <Link to="/signup">Register</Link>
+            </div>
+        </div>
+    </div>
+      
+    </div>
+  )
+}
+
+export default Navbar
